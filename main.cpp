dfbba6a9e2c8e36d01e38dce544f98128e4d4bab
garciafox <gianna.garcia@upr.edu>
2025-02-17 23:31:11 +0000

    programa para sumar fracciones

--- main.cpp
+++ main.cpp
@@ -4,7 +4,34 @@
 using namespace std;
 
 int main(){
-  // escriba su código aquí 
+  // intro del programa y so funcion
+  cout << "Este programa suma dos fracciones." << endl;
+
+  //declarar las variables 
+  double numerador1;
+  double denominador1;
+  double numerador2;
+  double denominador2;
+
+  //input de las variables
+  cout << "Entre el numerador de la 1ra fracción: ";
+  cin >> numerador1;
+
+  cout << "Entre el denominador de la 1ra fracción: ";
+  cin >> denominador1;
+
+  cout << "Entre el numerador de la 2da fracción: ";
+  cin >> numerador2;
+
+  cout << "Entre el denominador de la 2da fracción: ";
+  cin >> denominador2;
+
+  //calculando la fraccion
+  double numerador_suma = (numerador1*denominador2)+(denominador1*numerador2);
+  double denominador_suma = (denominador1*denominador2);
+
+  //produciendo el resulatado 
+  cout << "La suma es: " << numerador_suma << "/" << denominador_suma << endl;
   
   return 0;
 }
